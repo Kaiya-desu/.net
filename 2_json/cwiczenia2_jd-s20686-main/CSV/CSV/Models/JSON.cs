0975f28e7e26913d9a3ee9aaeaf84968f2c733c1
Karolina Strużek <60381425+Kaiya-desu@users.noreply.github.com>
2023-11-08 13:35:47 +0100

    Delete 2_json/cwiczenia2_jd-s20686-main/CSV directory

--- 2_json/cwiczenia2_jd-s20686-main/CSV/CSV/Models/JSON.cs
+++
@@ -1,7 +0,0 @@
-namespace CSV.Models
-{
-    public class JSON
-    {
-        public Uczelnia Uczelnia { get; set; }
-    }
-}
--- 2_json/cwiczenia2_jd-s20686-main/CSV/CSV/Models/Studies.cs
+++
@@ -1,8 +0,0 @@
-namespace CSV.Models
-{
-    public class Studies
-    {
-        public string Profil { get; set; }
-        public string Tryb { get; set; }
-    }
-}
