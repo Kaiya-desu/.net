e484148f7a7521c74d57648aaaf129e3d92984ab
Karolina Strużek <60381425+Kaiya-desu@users.noreply.github.com>
2023-11-08 13:53:47 +0100

    Add files via upload

---
+++ 11_WebApp/WebApplication/WebApplication/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApplication.Models
+{
+    public class Student
+    {
+        public string AvatarUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Studies { get; set; }
+    }
+}
